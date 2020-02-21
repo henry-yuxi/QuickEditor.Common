dbf7a3bb47bd126c77a8b448dab57e4544e348c6
Henry <henry890307@yeah.net>
2020-02-21 15:44:46 +0800

    新增ump上传bat脚本

---
+++ upm_updater.bat
@@ -0,0 +1,11 @@
+@echo off
+
+set PROJ_GIT_PATH=D:\GitProjects\QuickEditor.Common
+set UPM_PREFIX=QuickEditor.Common/Assets/QuickEditor/QuickEditor.Common
+set UPM_TAG=0.1.1
+d:
+cd /d %PROJ_GIT_PATH%
+git subtree split --prefix=%UPM_PREFIX% --branch upm
+git tag %UPM_TAG% upm       
+git push origin upm --tags
+@cmd.exe
